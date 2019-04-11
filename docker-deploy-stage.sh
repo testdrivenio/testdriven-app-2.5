2834c9ad22f55bc951311163d02188f1ab89d340
Michael Herman <hermanmu@gmail.com>
2019-04-11 15:42:03 -0600

    part 5 (wip)

--- docker-deploy-stage.sh
+++ docker-deploy-stage.sh
@@ -24,28 +24,43 @@ then
       fi
     }
 
+    update_service() {
+      if [[ $(aws ecs update-service --cluster $cluster --service $service --task-definition $revision | $JQ '.service.taskDefinition') != $revision ]]; then
+        echo "Error updating service."
+        return 1
+      fi
+    }
+
     deploy_cluster() {
 
+      cluster="test-driven-staging-cluster"
+
       # users
+      service="testdriven-users-stage-service"
       template="ecs_users_stage_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID $AWS_ACCOUNT_ID)
       echo "$task_def"
       register_definition
+      update_service
 
       # client
+      service="testdriven-client-stage-service"
       template="ecs_client_stage_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "$task_def"
       register_definition
+      update_service
 
       # swagger
+      service="testdriven-swagger-stage-service"
       template="ecs_swagger_stage_taskdefinition.json"
       task_template=$(cat "ecs/$template")
       task_def=$(printf "$task_template" $AWS_ACCOUNT_ID)
       echo "$task_def"
       register_definition
+      update_service
 
     }
 
