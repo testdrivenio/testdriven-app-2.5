256d55e63155a49327e37c0c21d3c0dc2164d8c9
Michael Herman <hermanmu@gmail.com>
2019-04-11 17:48:20 -0600

    part 5 (wip)

--- docker-push.sh
+++ docker-push.sh
@@ -8,6 +8,7 @@ then
     export REACT_APP_USERS_SERVICE_URL="http://testdriven-staging-alb-741461415.us-west-1.elb.amazonaws.com"
   elif [[ "$TRAVIS_BRANCH" == "production" ]]; then
     export DOCKER_ENV=prod
+    export REACT_APP_USERS_SERVICE_URL="http://testdriven-production-alb-770072310.us-west-1.elb.amazonaws.com"
   fi
 
   if [ "$TRAVIS_BRANCH" == "staging" ] || \
